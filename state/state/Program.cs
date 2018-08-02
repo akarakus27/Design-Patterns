d90220ae2238d17d1efeac2b8f6a3448f73b2428
Abdulkadir Karakuş <15541028@firat.edu.tr>
2018-08-02 13:51:42 +0300

    Add files via upload

---
+++ state/state/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace state
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Context context = new Context();
+            ModifedState modified = new ModifedState();
+            modified.DoAction(context);
+
+            DeletedState deleted = new DeletedState();
+            deleted.DoAction(context);
+
+           Console.WriteLine( context.GetState().ToString());
+
+            Console.ReadLine();
+
+        }
+    }
+    interface IState
+    {
+        void DoAction(Context context);
+    }
+    class ModifedState:IState
+    {
+        public void DoAction(Context context)
+        {
+            Console.WriteLine("State:Modified");
+            context.SetState(this);
+        }
+        public override string ToString()
+        {
+            return "Modified" ;
+        }
+    }
+    class DeletedState : IState
+    {
+        public void DoAction(Context context)
+        {
+            Console.WriteLine("State:Deleted");
+            context.SetState(this);
+        }
+        public override string ToString()
+        {
+            return "Deleted";
+        }
+
+    }
+    class AddedState : IState
+    {
+        public void DoAction(Context context)
+        {
+            Console.WriteLine("State:Added");
+            context.SetState(this);
+        }
+
+        public override string ToString()
+        {
+            return "Added";
+        }
+
+    }
+    class Context
+    {
+        private IState _state;
+        public void SetState(IState state)
+        {
+            _state = state;
+        }
+        public  IState GetState()
+        {
+            return _state;
+        }
+    }
+
+}
